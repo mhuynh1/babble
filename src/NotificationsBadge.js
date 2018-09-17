d69236b3ab292cfe04441f6c7fe53ad722de8aec
Mary Huynh <mhuynhh@gmail.com>
2018-09-17 10:50:30 -0700

    make NotificationsBadge stateless

--- src/NotificationsBadge.js
+++ src/NotificationsBadge.js
@@ -1,45 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
-import base from './base';
-class NotificationsBadge extends Component {
-    state = {
-        notifications: '',
-    }
-
-    componentDidMount() {
-        this.getLatestNotificationsCounts()
-    }
-
-    componentDidUpdate(_prevProps, prevState) {
-        if (prevState.notifications[this.props.user.uid] !== this.state.notifications[this.props.user.uid]) {
-            this.getLatestNotificationsCounts()
-        }
-    }
-
-    getLatestNotificationsCounts = () => {
-        if (this.notificationsRef) {
-            base.removeBinding(this.notificationsRef)
-        }
-
-        this.notificationsRef = base.syncState(`notifications/${this.props.roomName}`, {
-            context: this,
-            state: 'notifications',
-        })
-    }
-
-    render() {
-        const count = this.state.notifications[this.props.user.uid]
-        return (
-            <React.Fragment>
-                {
-                    count > 0
-                        ? <span className="Notifications" style={styles.span}>{count}</span>
-                        : null
-                }
-            </React.Fragment>
-        )
-    }
+const NotificationsBadge = ({ count }) => {
+    return (<span className="Notifications" style={styles.span}>{count}</span>)
 }
 
 const styles = {
@@ -54,7 +16,4 @@ const styles = {
     }
 }
 
-const mapStateToProps = state => {
-    return { user: state.user }
-}
-export default connect(mapStateToProps)(NotificationsBadge);
+export default NotificationsBadge;
